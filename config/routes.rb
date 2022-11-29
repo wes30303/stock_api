04fdf737abcd066a694bab6f35f2452a95609a77
Wesley Miranda <wesleywes42@gmail.com>
2022-11-29 00:26:04 -0700

    new stack

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :stock, only: %i[index]
-      root "welcome#index"
-      get '/forcast', to: 'forcast#index'
-      post 'users', to:'users#create'
-      post 'sessions', to:'sessions#create'
-      post 'road_trip', to:'road_trip#create'
     end
   end
 end
